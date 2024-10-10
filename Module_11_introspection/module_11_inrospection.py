f3d29b6f2b639249c99140923a7bbc96355df6da
Druseus <79805191+DRuseus@users.noreply.github.com>
2024-10-10 17:55:41 +0300

    Выполнено задание по теме "Интроспекция"

--- Module_11_introspection/module_11_inrospection.py
+++ Module_11_introspection/module_11_inrospection.py
@@ -1,10 +1,20 @@
 import inspect
 
 
+class Something:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        self.__C_CONST = float(3.14)
+
+    def get_a_b_c(self):
+        return self.a, self.b
+
+
 def introspection_info(obj):
     type_of_object = type(obj)
-    attribute = [attr for attr in dir(obj) if attr.isalnum()]
-    methods = [meth for meth in dir(obj) if not meth.isalnum()]
+    attribute = obj.__getstate__()
+    methods = [meth for meth in dir(obj)]
     module = inspect.getmodule(obj)
 
     __local_dict = locals()
@@ -12,5 +22,6 @@ def introspection_info(obj):
 
 
 if __name__ == '__main__':
-    number_info = introspection_info(42)
+    some_obj = Something('a', 42)
+    number_info = introspection_info(some_obj)
     print(number_info)
